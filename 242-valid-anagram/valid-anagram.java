8264f4079fef85e998dfb8acaa6edef791386e62
Ishant-singh123 <eshusingh114@gmail.com>
2026-09-26 14:56:37 +0530

    Time: 3 ms (93.81%) | Memory: 46.8 MB (8.81%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char []str1=s.toCharArray();
+        char []str2=t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        String s1=new String(str1);
+        String s2=new String(str2);
+        if(s1.equals(s2)){
+            return true;
+        }
+        return false;
+    }
+}
